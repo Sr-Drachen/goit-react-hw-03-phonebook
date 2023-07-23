5f075af5571fa0d3dc1560c731b9023324e1d9c9
Sr-Drachen <116126364+Sr-Drachen@users.noreply.github.com>
2023-07-23 02:02:38 -0500

    💣🎉🍎

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { Container, Title, SubTitle, Wrapper } from './components/App.styled';
-import ContactForm from './components/ContactForm';
-import ContactList from './components/ContactList';
-import Filter from './components/Filter';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import Filter from './components/Filter/Filter';
 
 class App extends Component {
   state = {
